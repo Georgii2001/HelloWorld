826312742493bef5c025fc872918cf8ee627aa16
Georgiana Voicea <georgiana.voicea2001@gmail.com>
2022-07-12 21:33:22 +0300

    conflict=1 added

---
+++ src/main/java/com/mkyong/Conflict.java
@@ -0,0 +1,12 @@
+package com.mkyong;
+
+import com.mkyong.MyWebApplication;
+import org.springframework.boot.SpringApplication;
+
+public class Conflict {
+    public static void main(String[] args) {
+     int conflict;
+     conflict=1;
+    }
+
+}
